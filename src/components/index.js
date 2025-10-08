4b5efaf968171e6c9d130a169ed7113f7cfedbf6
Arif <117912574+RogerZoe@users.noreply.github.com>
2025-10-08 20:52:47 +0530

    Delete src directory

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { Header } from './Header';
-export { WalletConnection } from './WalletConnection';
-export { CreateExpense } from './CreateExpense';
-export { PayShare } from './PayShare';
-export { StatusMessage } from './StatusMessage';
